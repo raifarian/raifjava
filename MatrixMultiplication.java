08672cb0986e5c21378af42af365be1da73b8416
raifarian <raifarian7@gmail.com>
2026-02-25 20:16:39 +0530

    Add files via upload

---
+++ MatrixMultiplication.java
@@ -0,0 +1,33 @@
+class MatrixMultiplication {
+    public static void main(String[] args) {
+
+        int[][] m1 = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int[][] m2 = {
+                {7, 8},
+                {9, 10},
+                {11, 12}
+        };
+
+        int[][] result = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 3; k++) {
+                    result[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+
+        System.out.println("Resultant Matrix:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
