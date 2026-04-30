ee37171d0d3a9913e1f6244eac22f5f8cbce3e54
raifarian <raifarian7@gmail.com>
2026-04-30 16:49:09 +0530

    Add files via upload

---
+++ CaseChange.java
@@ -0,0 +1,13 @@
+public class CaseChange {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLowerCase(ch)) result += Character.toUpperCase(ch);
+            else if (Character.isUpperCase(ch)) result += Character.toLowerCase(ch);
+            else result += ch;
+        }
+        System.out.println(result);
+    }
+}
